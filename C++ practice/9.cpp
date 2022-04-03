793dfe34535f7fa8e365e8ea0085a8b1696a6bee
Jonil <jonil.shingala@gmail.com>
2022-04-03 22:15:10 +0530

    loop

---
+++ C++ practice/9.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+
+using namespace std;
+int main()
+{
+  
+
+
+     int i=1;
+     cout<<i;
+     i++;
+
+
+   
+
+    for (int i = 1; i <= 40; i++)
+    {
+       
+        cout<<i<<endl;
+    }
+
+  
+    for (int i = 1; 34 <= 40; i++)
+    {
+        cout<<i<<endl;
+    }
+
+
+
+
+    int i=1;
+    while(i<=40){
+        cout<<i<<endl;
+        i++;
+    }
+
+    int i = 1;
+    while (true)
+    {
+        cout << i << endl;
+        i++;
+    }
+
+
+  
+   
+    int i=1;
+    do{
+        cout<<i<<endl;
+        i++;
+    }while(false);
+
+    return 0;
+}
