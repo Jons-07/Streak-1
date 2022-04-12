cd3c441fc433475ee940c14c239869b8ee6371e1
Jonil <jonil.shingala@gmail.com>
2022-04-12 21:54:33 +0530

    C++

---
+++ C++ practice/23_Constructer.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+using namespace std;
+
+class Complex
+{
+    int a, b;
+
+public:
+    
+
+    Complex(void);
+
+    void printNumber()
+    {
+        cout << "Your number is " << a << " + " << b << "i" << endl;
+    }
+};
+
+Complex ::Complex(void) 
+{
+    a = 10;
+    b = 0;
+   
+}
+int main()
+{
+    Complex c1, c2, c3;
+    c1.printNumber();
+    c2.printNumber();
+    c3.printNumber();
+
+    return 0;
+}
+
+
+class Point{
+    int x, y;
+    public:
+        Point(int a, int b){
+            x = a;
+            y = b;
+        }
+
+        void displayPoint(){
+            cout<<"The point is ("<<x<<", "<<y<<")"<<endl;
+        }
+
+};
