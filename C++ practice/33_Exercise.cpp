413d1f3e161d07a7a975eb690e0b65a6fbde4eb6
Jonil <jonil.shingala@gmail.com>
2022-04-15 22:23:13 +0530

    C++

---
+++ C++ practice/33_Exercise.cpp
@@ -0,0 +1,31 @@
+class ScientificCalculator{
+    int a, b;
+
+    public:
+        void getDataScientific()
+        {
+            cout << "Enter the value of a" << endl;
+            cin >> a;
+            cout << "Enter the value of b" << endl;
+            cin >> b;
+        }
+
+        void performOperationsScientific()
+        { 
+            cout << "The value of cos(a) is: " << cos(a) << endl;
+            cout << "The value of sin(a) is: " << sin(a) << endl;
+            cout << "The value of exp(a) is: " << exp(a) << endl;
+            cout << "The value of tan(a) is: " << tan(a) << endl;
+        }
+};
+
+int main()
+{
+    HybridCalculator calc;
+    calc.getDataScientific();
+    calc.performOperationsScientific();
+    calc.getDataSimple();
+    calc.performOperationsSimple();
+    
+    return 0;
+}
