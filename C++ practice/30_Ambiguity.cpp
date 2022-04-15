577d746350ecd1b6d646439ecac942d6eccf7fd4
Jonil <jonil.shingala@gmail.com>
2022-04-15 22:21:19 +0530

    C++

---
+++ C++ practice/30_Ambiguity.cpp
@@ -0,0 +1,24 @@
+
+class Base1{
+    public:
+        void greet(){
+            cout<<"How are you?"<<endl;
+        }
+};
+
+class Base2{
+    public:
+        void greet()
+        {
+            cout << "Kaise ho?" << endl;
+        }
+};
+
+
+class Derived : public Base1, public Base2{
+   int a;
+   public:
+    void greet(){
+        Base2 :: greet();
+    }
+};
