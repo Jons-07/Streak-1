ad757244a9c16f1512e3117e66cbfe4b0b6a8b57
Jonil <jonil.shingala@gmail.com>
2022-04-18 22:05:08 +0530

    Python

---
+++ Python/Ch2_VAriable&Datatypes.py
@@ -0,0 +1,28 @@
+a=350
+
+b="jon"
+
+c=715.22
+
+a = 71                                    #Identifies a as class<int>
+
+b = 88.44                              #Identifies b as class<float>
+
+name = "abc"                  #Identifies name as class<Str>
+
+a = 31
+
+type(a)                      #class<int>
+
+b = “31”
+
+type(b)                      #class<str>
+
+Str(31)           # ”31” Integer to string conversion
+
+int(“32”)       # 32 String to int conversion
+
+
+float(32)       #32.0 Integer to float conversion
+
+a = input(“Enter name”)              
---
+++ Python/Ch3_Strings.py
@@ -0,0 +1,9 @@
+word = “amazing”
+
+word[1:6:2]          
+
+word = "amazing"
+
+word[:7] or word[0:7]      #It will return ‘amazing’
+
+word[0:] or word[0:7]      #It will return ‘amazing’
